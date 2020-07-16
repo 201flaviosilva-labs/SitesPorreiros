a29c2aeaa828ec0d4e796d2d0238fbb2e60aa9f4
Flávio Silva <flavioandre.2001.9.7@gmail.com>
2020-07-16 17:45:05 +0100

    Por as imagens online (imgbb)

--- Pages/Ninica/animation.js
+++ Pages/Ninica/animation.js
@@ -1,10 +1,16 @@
-const listaImagens = ["Ninica1.jpeg", "Ninica2.jpeg", "Ninica3.jpeg"];
+const listaImagens = ["https://i.ibb.co/zV16KJK/Ninica1.jpg",
+  "https://i.ibb.co/dcv3H6f/Ninica2.jpg",
+  "https://i.ibb.co/JvWznQ5/Ninica3.jpg",
+  "https://i.ibb.co/w7yXqZc/Ninica4.jpg",
+  "https://i.ibb.co/mFKxfj3/Ninca5.jpg",
+  "https://i.ibb.co/NxM7m18/Ninica6.jpg",
+  "https://i.ibb.co/TLJK9rc/Ninica7.jpg"
+];
 
 document.getElementById("MudarE").addEventListener("click", () => novaImgs());
 document.getElementById("MudarD").addEventListener("click", () => novaImgs());
 
 function novaImgs() {
   const numRandom = Math.floor(Math.random() * listaImagens.length);
-  document.getElementById("ImgNinica").src =
-    "./Assets/" + listaImagens[numRandom];
+  document.getElementById("ImgNinica").src = listaImagens[numRandom];
 }
