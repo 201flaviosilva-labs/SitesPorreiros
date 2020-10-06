ecbde927cbd0775b56e2402099d91204a2e435cc
Flavio Silva <flavioandre.2001.9.7@gmail.com>
2020-10-06 11:44:36 +0100

    Adicionar Mais Dias

--- Pages/TempoSobra/script.js
+++ Pages/TempoSobra/script.js
@@ -1,13 +1,19 @@
-const DiaSpan = document.getElementById("Dia");
+const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const msPerDay = 24 * 60 * 60 * 1000; // 86 400 000
 
 setInterval(() => {
+	document.getElementById("Natal").innerHTML = calcular(25, month[11]);
+	document.getElementById("Halloween").innerHTML = calcular(31, month[9]);
+	document.getElementById("AnoNovo").innerHTML = calcular(31, month[11]);
+}, 1000);
+
+
+function calcular(dia, mes) {
 	const today = new Date();
-	const xmas = new Date("December 24, " + today.getFullYear());
+	const xmas = new Date(`${mes} ${dia} ${today.getFullYear()}`);
 	if (today > xmas) xmas.setYear(today.getFullYear() + 1);
 
-	msPerDay = 24 * 60 * 60 * 1000; // 86 400 000
-	timeLeft = (xmas.getTime() - today.getTime());
-	daysLeft = Math.floor(timeLeft / msPerDay);
-
-	DiaSpan.innerHTML = daysLeft;
-}, 1000);
+	console.log(xmas);
+	const timeLeft = (xmas.getTime() - today.getTime());
+	return Math.floor(timeLeft / msPerDay);
+}
