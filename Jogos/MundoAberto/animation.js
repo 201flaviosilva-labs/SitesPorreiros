59809db2eb3ccf9e8936168b2a2b2bb67ca0b56f
Flavio Silva <flavioandre.2001.9.7@gmail.com>
2020-10-30 19:02:58 +0000

    Juntar o melhor dos dois "Mundos" com um Random

--- Jogos/MundoAberto/animation.js
+++ Jogos/MundoAberto/animation.js
@@ -18,13 +18,29 @@ function desenhar() {
 	jogador.fillRect(jogadorX, jogadorY, jogadorWidth, jogadorHeight);
 }
 
+const passarParede = Math.random() > 0.5;
 document.addEventListener("keydown", (e) => moverJogador(e));
-
 function moverJogador(e) {
 	const keyCode = e.keyCode;
+	passarParede ? loopWall(keyCode) : stopWall(keyCode);
+	desenhar();
+}
+
+function stopWall(keyCode) {
 	if (keyCode === 37 && jogadorX >= jogadorWidth) jogadorX -= velocidade;
 	if (keyCode === 39 && jogadorX <= canvasWidth - jogadorWidth - velocidade) jogadorX += velocidade;
 	if (keyCode === 38 && jogadorY >= jogadorHeight) jogadorY -= velocidade;
 	if (keyCode === 40 && jogadorY <= canvasHeight - jogadorHeight - velocidade) jogadorY += velocidade;
-	desenhar();
+}
+
+function loopWall(keyCode) {
+	if (keyCode === 37) jogadorX -= velocidade;
+	if (keyCode === 39) jogadorX += velocidade;
+	if (keyCode === 38) jogadorY -= velocidade;
+	if (keyCode === 40) jogadorY += velocidade;
+
+	if (jogadorX < jogadorWidth - velocidade) jogadorX = canvasWidth - jogadorWidth;
+	if (jogadorX > canvasWidth - jogadorWidth) jogadorX = 0;
+	if (jogadorY < jogadorHeight - velocidade) jogadorY = canvasHeight - jogadorHeight;
+	if (jogadorY > canvasHeight - jogadorHeight) jogadorY = 0;
 }
