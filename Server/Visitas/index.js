b39080e2a83c7efac71f210f364d8e6ff127f1f4
Flavio Silva <flavioandre.2001.9.7@gmail.com>
2020-12-27 18:33:57 +0000

    Visitas Servidor

---
+++ Server/Visitas/index.js
@@ -0,0 +1,18 @@
+const express = require("express")();
+const storage = require("node-persist");
+
+let counter = 0;
+
+express.get("/", (req, res) => {
+	counter++;
+	storage.setItem("counter", counter).then(() => {
+		res.json(counter);
+	});
+});
+
+storage.init().then(() => storage.getItem("counter")).then((value) => {
+	if (value > 0) counter = value;
+	else counter = 0;
+
+	// express.listen(5000, "0.0.0.0"); // Dev
+});
