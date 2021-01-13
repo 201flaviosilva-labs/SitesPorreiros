4f5e38b2ab26d1b79c6cc5184ccfa4752e1373ac
Flavio Silva <flavioandre.2001.9.7@gmail.com>
2021-01-13 12:22:00 +0000

    Illuminação

--- Pages/Iluminacao/animation.js
+++ Pages/Iluminacao/animation.js
@@ -9,14 +9,14 @@ let flag = false;
 btn.addEventListener("click", () => {
   if (flag) {
     // Ligado
-    main.style.backgroundColor = "black";
+    document.body.style.backgroundColor = "black";
     h1Text.innerHTML = "Ilumina-me!";
     h1Text.style.color = "white";
     imagenImg.src = "./Lampada/lightoff.svg";
     btnOn.innerHTML = "Ligar";
   } else {
     // Desligado
-    main.style.backgroundColor = "white";
+    document.body.style.backgroundColor = "white";
     h1Text.innerHTML = "Escurece-me";
     h1Text.style.color = "black";
     imagenImg.src = "./Lampada/lighton.svg";
